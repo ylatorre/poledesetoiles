2101b57e2974bb555211ea88b13929f68cb35224
NE0G0RE <91892411+NE0G0RE@users.noreply.github.com>
2022-04-22 11:53:17 +0200

    1eressaicarteceles

--- resources/views/CarteCeleste5.blade.php
+++ resources/views/CarteCeleste5.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <iframe width="1920" height="900" style="position: fixed; left: 0px; top: 0px;"  scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/index.html?longitude=4.798577016906673&latitude=45.77070341094022&projection=mollweide&constellations=true&constellationlabels=true&meteorshowers=true&showstarlabels=true&az=302" allowTransparency="true"></iframe>
-{{--<iframe width="1920" height="1080"   "frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/index.html?longitude=2.2553567&latitude=48.9008079&projection=mollweide&constellations=true&constellationlabels=true&meteorshowers=true&live=true&az=295.0286134844232" allowTransparency="true"></iframe>--}}
+{{--<iframe width="1920" height="1080"   frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/index.html?longitude=2.2553567&latitude=48.9008079&projection=mollweide&constellations=true&constellationlabels=true&meteorshowers=true&live=true&az=295.0286134844232" allowTransparency="true"></iframe>--}}
 {{--<iframe width="1920" height="1080"  style="position: fixed; left: 0px; top: 0px;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/index.html?longitude=2.2553567&latitude=48.9008079&projection=stereo&constellations=true&constellationlabels=true&meteorshowers=true&live=true&az=137.7786134844232" allowTransparency="true"></iframe>--}}
 {{--<iframe width="1920" height="1080" style="position: fixed; left: 0px; top: 0px;"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/index.html?longitude=-119.86286000000001&latitude=34.4326&projection=stereo&constellations=true&constellationlabels=true&meteorshowers=true&live=true&az=349.2786134844232" allowTransparency="true"></iframe>--}}
 </body>
@@ -27,9 +27,9 @@
 {{--<script type="javascript">--}}
 {{--    var planetarium;--}}
 
-{{--    S(document).ready(function() {--}}
+{{--    $(document).ready(function() {--}}
 
-{{--        planetarium = S.virtualsky({--}}
+{{--        planetarium = $.virtualsky({--}}
 {{--            id: 'starmap'	// This should match the ID used in the DOM--}}
 {{--        });--}}
 
