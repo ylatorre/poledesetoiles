5a58df59f1dea3e167d4b0a7776cb572b614dcbc
NE0G0RE <91892411+NE0G0RE@users.noreply.github.com>
2022-04-22 17:37:29 +0200

    Update Planetarium.blade.php

--- resources/views/Planetarium.blade.php
+++ resources/views/Planetarium.blade.php
@@ -14,4 +14,44 @@
             </div>
         </div>
     </div>
+    
+    {{--    tu peux ecrir a partir d'ici--}}
+
+    <section>
+        <div id="starmapgnomic" style="width:944px;height:400px;"></div>
+    </section>
+
+
+    {{--ici le css--}}
+    <style>
+
+    </style>
+
+    {{--    ici le js--}}
+
+    <script type="text/javascript">
+
+        $(document).ready(function() {
+
+            var planetarium = $.virtualsky({
+                'id': 'starmapgnomic',
+                'projection': 'gnomic',
+                'longitude': 4.798577016906673,
+                'latitude': 45.77070341094022,
+                'mouse': true,
+                'cardinalpoints': true,
+                'constellations': true,
+                'constellationlabels': true,
+                'meteorshowers': true,
+                'showplanets': true,
+                'showplanetlabels': true,
+                'showstarlabels': true,
+                'showstars': true,
+                'live': true,
+                'fov': 200
+            });
+
+        });
+
+    </script>
 </x-app-layout>
