dd96022785e8b43e20f72ed566742a1f4df0441a
yvan <yvanlatorre@hotmail.fr>
2022-05-05 16:43:02 +0200

    modif slider

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -1,94 +1,25 @@
 <x-app-layout >
+    <!-- Font Awesome -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css"
+        rel="stylesheet"
+    />
+    <!-- MDB -->
+    <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"
+    ></script>
 
-    <div
-        id="carouselDarkVariant"
-        class="carousel slide carousel-fade carousel-dark relative"
-        data-bs-ride="carousel"
-    >
-        <!-- Indicators -->
-        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-            <button
-                data-bs-target="#carouselDarkVariant"
-                data-bs-slide-to="0"
-                class="active"
-                aria-current="true"
-                aria-label="Slide 1"
-            ></button>
-            <button
-                data-bs-target="#carouselDarkVariant"
-                data-bs-slide-to="1"
-                aria-label="Slide 1"
-            ></button>
-            <button
-                data-bs-target="#carouselDarkVariant"
-                data-bs-slide-to="2"
-                aria-label="Slide 1"
-            ></button>
-        </div>
-
-        <!-- Inner -->
-        <div class="carousel-inner relative w-full overflow-hidden">
-            <!-- Single item -->
-            <div class="carousel-item active relative float-left w-full">
-                <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
-                    class="block w-full"
-                    alt="Motorbike Smoke"
-                />
-                <div class="carousel-caption hidden md:block absolute text-center">
-                    <h5 class="text-xl">First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-
-            <!-- Single item -->
-            <div class="carousel-item relative float-left w-full">
-                <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
-                    class="block w-full"
-                    alt="Mountaintop"
-                />
-                <div class="carousel-caption hidden md:block absolute text-center">
-                    <h5 class="text-xl">Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
-            </div>
-
-            <!-- Single item -->
-            <div class="carousel-item relative float-left w-full">
-                <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
-                    class="block w-full"
-                    alt="Woman Reading a Book"
-                />
-                <div class="carousel-caption hidden md:block absolute text-center">
-                    <h5 class="text-xl">Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
-                </div>
-            </div>
-        </div>
-        <!-- Inner -->
-
-        <!-- Controls -->
-        <button
-            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-            type="button"
-            data-bs-target="#carouselDarkVariant"
-            data-bs-slide="prev"
-        >
-            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-            type="button"
-            data-bs-target="#carouselDarkVariant"
-            data-bs-slide="next"
-        >
-            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -133,6 +64,102 @@
 
         </div>
     </div>
+{{--        video carousel--}}
+        <div
+            id="carouselVideoExample"
+            class="carousel slide carousel-fade"
+            data-mdb-ride="carousel"
+        >
+            <!-- Indicators -->
+            <div class="carousel-indicators">
+                <button
+                    type="button"
+                    data-mdb-target="#carouselVideoExample"
+                    data-mdb-slide-to="0"
+                    class="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                ></button>
+                <button
+                    type="button"
+                    data-mdb-target="#carouselVideoExample"
+                    data-mdb-slide-to="1"
+                    aria-label="Slide 2"
+                ></button>
+                <button
+                    type="button"
+                    data-mdb-target="#carouselVideoExample"
+                    data-mdb-slide-to="2"
+                    aria-label="Slide 3"
+                ></button>
+            </div>
+
+            <!-- Inner -->
+            <div class="carousel-inner">
+                <!-- Single item -->
+                <div class="carousel-item active">
+                    <video  style="width: 100%; height: auto;" autoplay loop muted>
+                        <source src="https://mdbcdn.b-cdn.net/img/video/Tropical.mp4" type="video/mp4" />
+                    </video>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>
+                            Nulla vitae elit libero, a pharetra augue mollis interdum.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Single item -->
+                <div class="carousel-item">
+                    <video  style="width: 100%; height: auto;" autoplay loop muted>
+                        <source src="https://mdbcdn.b-cdn.net/img/video/forest.mp4" type="video/mp4" />
+                    </video>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Single item -->
+                <div class="carousel-item">
+                    <video style="width: 100%; height: auto;" autoplay loop muted>
+                        <source
+                            src="https://mdbcdn.b-cdn.net/img/video/Agua-natural.mp4"
+                            type="video/mp4"
+                        />
+                    </video>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Inner -->
+
+            <!-- Controls -->
+            <button
+                class="carousel-control-prev"
+                type="button"
+                data-mdb-target="#carouselVideoExample"
+                data-mdb-slide="prev"
+            >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                class="carousel-control-next"
+                type="button"
+                data-mdb-target="#carouselVideoExample"
+                data-mdb-slide="next"
+            >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     <x-slot name="header" style="display: none; visibility: hidden;">
         {{--        <h2 class="font-semibold text-xl text-gray-800 leading-tight ">--}}
         {{--            {{ __('Accueil') }}--}}
