881d02d6289f1b2ccde3be724f84e2a697cb258f
NE0G0RE <91892411+NE0G0RE@users.noreply.github.com>
2022-05-17 15:48:33 +0200

    Update Accueil.blade.php

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -166,7 +166,7 @@
                         <div class="p-6">
                             <h5 class="text-gray-900 text-xl font-medium mb-2">Carte Cesleste Gnomic</h5>
                             <p class="text-gray-700 text-base mb-4">
-                                Vue de la carte stellaire en mode gnomic à la position géographique du Pôle de Etoiles, hémisphère nord et sud avec zoom possible .
+                                Carte stellaire en mode vue gnomic à la position géographique du Pôle de Etoiles, hémisphère nord et sud avec zoom possible .
                             </p>
                             <a href="{{ route('CarteCeleste') }}">
                             <button type="button"  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
@@ -184,7 +184,7 @@
                         <div class="p-6">
                             <h5 class="text-gray-900 text-xl font-medium mb-2">Carte Cesleste Stereo</h5>
                             <p class="text-gray-700 text-base mb-4">
-                                Vue de la carte stellaire en mode gnomic à la position géographique du Pôle de Etoiles, hémisphère nord et sud avec zoom possible .
+                                Carte stellaire en mode horizon visible à la position géographique du Pôle de Etoiles, .
                             </p>
                             </p>
                             <a href="{{ route('CarteCeleste4') }}">
