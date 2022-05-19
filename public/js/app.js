38228afb25292b6a15d4848b9a6711bc3e91d827
yvan <yvanlatorre@hotmail.fr>
2022-05-19 12:34:22 +0200

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -22349,6 +22349,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/scss/scss2.scss":
+/*!***********************************!*\
+  !*** ./resources/scss/scss2.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/css/app.css":
 /*!*******************************!*\
   !*** ./resources/css/app.css ***!
@@ -22703,6 +22716,7 @@ process.umask = function() { return 0; };
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
 /******/ 			"css/app": 0,
+/******/ 			"scss/scss2": 0,
 /******/ 			"scss/scss": 0
 /******/ 		};
 /******/ 		
@@ -22753,9 +22767,10 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app","scss/scss"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/app","scss/scss"], () => (__webpack_require__("./resources/scss/scss.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","scss/scss"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","scss/scss2","scss/scss"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","scss/scss2","scss/scss"], () => (__webpack_require__("./resources/scss/scss.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","scss/scss2","scss/scss"], () => (__webpack_require__("./resources/scss/scss2.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","scss/scss2","scss/scss"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
