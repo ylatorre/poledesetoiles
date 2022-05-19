09b3bb548fbc6c86e2e7f782a7f365f8fd8bdaf1
NE0G0RE <91892411+NE0G0RE@users.noreply.github.com>
2022-05-19 22:26:39 +0200

    Update Calendrier.blade.php

--- resources/views/Calendrier.blade.php
+++ resources/views/Calendrier.blade.php
@@ -309,9 +309,6 @@
     }
     </script>
     </div>
-    <script type="text/javascript">
-        document.getElementsByClassName("ytp-title-text").class = display: none;
-    </script>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
